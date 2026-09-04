cda4f2ecd80a09d569983c4f2de8c67972558774
tileflower <shylla488@gmail.com>
2026-09-04 00:42:21 -0400

    Items and Blocks!

--- forge-1.21-51.0.33-mdk/src/main/java/net/tileflower/tutorialmod/TutorialMod.java
+++ forge-1.21-51.0.33-mdk/src/main/java/net/tileflower/tutorialmod/TutorialMod.java
@@ -1,6 +1,7 @@
 package net.tileflower.tutorialmod;
 
 import com.mojang.logging.LogUtils;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -13,6 +14,8 @@ import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.tileflower.tutorialmod.block.ModBlocks;
+import net.tileflower.tutorialmod.item.Moditems;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
@@ -33,6 +36,11 @@ public class TutorialMod
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
 
+
+        Moditems.register(modEventBus); // register items, see Moditems
+        ModBlocks.register(modEventBus); // register blocks, see ModBlocks
+
+
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
 
@@ -45,9 +53,16 @@ public class TutorialMod
     }
 
     // Add the example block item to the building blocks tab
-    private void addCreative(BuildCreativeModeTabContentsEvent event)
-    {
+    private void addCreative(BuildCreativeModeTabContentsEvent event) {
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS) { //adds item to ingredients tab of creative  mode
+            event.accept(Moditems.ALEXANDRITE);
+            event.accept(Moditems.RAW_ALEXANDRITE);
+        }
+        if(event.getTabKey() == CreativeModeTabs.BUILDING_BLOCKS) { //adds item to ingredients tab of creative  mode
+            event.accept(ModBlocks.ALEXANDRITE_BLOCK);
+            event.accept(ModBlocks.RAW_ALEXANDRITE_BLOCK);
 
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
